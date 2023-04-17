c7d12000b4d2b451879649277a5a58bdb613530b
Daegyeom Song <LeoSong7077@gmail.com>
2023-04-17 19:29:37 +0900

    index 페이지, tags 페이지

---
+++ api/routes/tags.js
@@ -0,0 +1,9 @@
+const express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res) {
+    res.render("tags", { title: "Express!!!" });
+});
+
+module.exports = router;
--- loaders/router.js
+++ loaders/router.js
@@ -1,5 +1,7 @@
 const indexRouter = require("../api/routes/index");
+const tagsRouter = require("../api/routes/tags");
 
 module.exports = function (app) {
     app.use("/", indexRouter);
+    app.use("/tags", tagsRouter);
 };
